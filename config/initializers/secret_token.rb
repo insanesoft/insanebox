6eb7a51b118c2b12482f3cfada85b64af1ccdbc3
Carlos Contreras Llinares <llinacont@gmail.com>
2013-12-17 20:21:48 +0100

    InsaneCommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Insanebox::Application.config.secret_key_base = '6853a55f138b42942e213ba9ae5825904926fb0c7c5467c559bdc1bccc92f9c07ee9b0a1271dcc62aba0feca11277910e974acdcbe9b9221c0472eeb2c4c100e'
